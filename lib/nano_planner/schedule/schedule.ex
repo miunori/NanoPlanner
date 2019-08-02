0daa9511926096f682eb7a8eda1e3c5f649ca365
miunori <miunori@yahoo.co.jp>
2019-08-02 16:58:57 +0900

    plan_item.ex の機能の一部を schedule.ex へと委譲

--- lib/nano_planner/schedule/schedule.ex
+++ lib/nano_planner/schedule/schedule.ex
@@ -1,3 +1,19 @@
 defmodule NanoPlanner.Schedule do
-  
+    alias NanoPlanner.Schedule.PlanItem
+
+    def convert_datetime(items) when is_list(items) do
+        Enum.map items, fn(item) ->
+            convert_datetime(item)
+        end
+    end
+    
+    def convert_datetime(%PlanItem{} = item) do
+        alias Timex.Timezone
+        time_zone = Application.get_env(:nano_planner, :default_time_zone)
+    
+        Map.merge(item, %{
+          starts_at: Timezone.convert(item.starts_at, time_zone),
+          ends_at: Timezone.convert(item.ends_at, time_zone)
+        })
+    end
 end
