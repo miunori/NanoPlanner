44a685de20ba13a92a7f8601c9ad54ee9d5cabcc
miunori <miunori@yahoo.co.jp>
2019-08-02 16:56:43 +0900

    plan_item.ex の場所を lib/nano_planner/schedule へと移動

---
+++ lib/nano_planner/schedule/plan_item.ex
@@ -0,0 +1,21 @@
+defmodule NanoPlanner.Schedule.PlanItem do
+  use Ecto.Schema
+  import Ecto.Changeset
+  #alias NanoPlanner.Schedule.PlanItem
+
+  schema "plan_items" do
+    field :name, :string
+    field :description, :string
+    field :starts_at, NanoPlanner.Ecto.Datetime
+    field :ends_at, NanoPlanner.Ecto.Datetime
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(plan_item, attrs) do
+    plan_item
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
